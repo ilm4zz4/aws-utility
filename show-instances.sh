19602df2ab7f7523e4dfcce7417b5168456b36e2
Michele-R <michele.rosellini@garmin.com>
2022-04-07 11:33:35 +0200

    Add show-instances.sh

---
+++ show-instances.sh
@@ -0,0 +1,3 @@
+aws ec2 describe-instances \
+--filters Name=tag-key,Values=Name \
+--query "Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key=='Name']|[0].Value,State:State.Name}" --output table
